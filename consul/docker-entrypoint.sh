0ba4bea3ad9da0fcff8e996a3977ef994a23edc8
Christophe Combelles <ccomb@free.fr>
2017-03-03 17:33:38 +0000

    reload caddy and haproxy after regenerating templates

--- consul/docker-entrypoint.sh
+++ consul/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 chown consul: /deploy
 
 /bin/consul-template \
-    -template="/consul/template/caddy/Caddyfile.ctmpl:/consul/template/caddy/Caddyfile" \
-    -template="/consul/template/haproxy/haproxy.cfg.ctmpl:/consul/template/haproxy/haproxy.cfg" &
+    -template="/consul/template/caddy/Caddyfile.ctmpl:/consul/template/caddy/Caddyfile:docker kill -s USR1 caddy_server_1" \
+    -template="/consul/template/haproxy/haproxy.cfg.ctmpl:/consul/template/haproxy/haproxy.cfg:docker kill -s HUP haproxy_server_1" &
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
