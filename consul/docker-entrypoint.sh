96393853ed9ac308761e2f7953888aa56d2b94ad
Christophe Combelles <ccomb@free.fr>
2017-03-03 21:12:04 +0000

    for now we don't signal but restart, this seems more reliable

--- consul/docker-entrypoint.sh
+++ consul/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 chown consul: /deploy
 
 /bin/consul-template \
-    -template="/consul/template/caddy/Caddyfile.ctmpl:/consul/template/caddy/Caddyfile:docker kill -s USR1 caddy_server_1" \
-    -template="/consul/template/haproxy/haproxy.cfg.ctmpl:/consul/template/haproxy/haproxy.cfg:docker kill -s HUP haproxy_server_1" &
+    -template="/consul/template/caddy/Caddyfile.ctmpl:/consul/template/caddy/Caddyfile:docker restart caddy_server_1" \
+    -template="/consul/template/haproxy/haproxy.cfg.ctmpl:/consul/template/haproxy/haproxy.cfg:docker restart haproxy_server_1" &
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
