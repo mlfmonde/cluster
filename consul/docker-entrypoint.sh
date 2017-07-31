f8eb0fe99afa4cd6c1bf8541d5f151f64b1a781c
Christophe Combelles <ccomb@free.fr>
2017-07-31 15:42:35 +0200

    don't block consul startup

--- consul/docker-entrypoint.sh
+++ consul/docker-entrypoint.sh
@@ -6,7 +6,7 @@ chown consul: /deploy
 caddytemplate="/consul/template/caddy/Caddyfile.ctmpl:/consul/template/caddy/Caddyfile:/reload_caddy.sh"
 haproxytemplate="/consul/template/haproxy/haproxy.cfg.ctmpl:/consul/template/haproxy/haproxy.cfg:/reload_haproxy.sh"
 
-/bin/consul-template -once -template=$caddytemplate -template=$haproxytemplate
+/bin/consul-template -once -template=$caddytemplate -template=$haproxytemplate &
 /bin/consul-template       -template=$caddytemplate -template=$haproxytemplate &
 
 # adapt the docker group of the container to the outside
