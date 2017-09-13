125d674fa6321a9b8e395d8117512907e718f2c2
Pierre Verkest <pverkest@anybox.fr>
2017-09-13 22:01:55 +0200

    #9385: reuse kill USR1 instead restart to relaod caddy conf

--- consul/reload_caddy.sh
+++ consul/reload_caddy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# Le signal USR1 met caddy dans un état incorrect (à creuser)
-#docker kill -s USR1 cluster_caddy_1 || docker restart cluster_caddy_1
-docker restart cluster_caddy_1
+docker kill -s USR1 cluster_caddy_1 || docker restart cluster_caddy_1
