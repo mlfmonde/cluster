217691cb17687b24b724cf332f6dfc57cdcfdd66
Pierre Verkest <pverkest@anybox.fr>
2017-09-19 22:57:37 +0200

    move to restart caddy as it still failing

--- consul/reload_caddy.sh
+++ consul/reload_caddy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker kill -s USR1 cluster_caddy_1 || docker restart cluster_caddy_1
+# Le signal USR1 met caddy dans un état incorrect (à creuser)
+# docker kill -s USR1 cluster_caddy_1 || docker restart cluster_caddy_1
+docker restart cluster_caddy_1
