98878c1273791ef758981e6e3e8fbfdefc7faa80
pyseed <pyseed.dev@gmail.com>
2018-10-18 17:57:28 +0200

    [IMP] dind: stop node script

---
+++ dind/stop.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+thisDir=$(dirname "$0")
+. "${thisDir}/config"
+. "${thisDir}/lib.bash"
+
+for index in ${nodes[*]}
+do
+    echo "stopping node ${index}..."
+
+    # we use specific compose override file for consul config
+    docker-compose exec "${nodeServicePrefix}${index}" docker-compose -f docker-compose.yml -f docker-compose.dind.yml stop
+done
+
+docker-compose stop
+
+for index in ${nodes[*]}
+do
+    mountDown "${index}"
+done
