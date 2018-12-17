4eca20a238f62745ecdf2776d9c2e6abb66af39a
vgreiner <vgreiner@anybox.fr>
2018-12-17 14:44:51 +0100

    [IMP] upgrade script: test u minor updates and comments

--- dind/ci/tests/test_update_script.py
+++ dind/ci/tests/test_update_script.py
@@ -14,7 +14,10 @@ class UpdateScriptBase(base_case.ClusterTestCase):
     migrate_test_file_path = '/tmp/migrate.txt'
 
     def _is_file_deployed(self, file_name):
-        # check that update.sh is deployed on master
+        """
+        check that given file is deployed
+        :param file_name: file name (no path)
+        """
         file_path = os.path.join(
             cluster.DEPLOY_ROOT_DIR,
             "{app_id}-{deploy_id}".format(
@@ -31,8 +34,11 @@ class UpdateScriptBase(base_case.ClusterTestCase):
 
     def _get_test_file_content(self, file_path, clean_up_after=True):
         """
-        check that we pass in update.sh script
-        update.sh of cluster_lab_test_service will write in 'test_file_path'
+        get content of a given test file
+        :param file_path: absolute path
+        :param clean_up_after: True to immediatly clean file after check
+        :return file content
+        :rtype utf-8 str
         """
         test_container = self.cluster.nodes[self.master][
             'docker_cli'
@@ -63,6 +69,12 @@ class UpdateScriptBase(base_case.ClusterTestCase):
         return res or ''
 
     def _clean_up_test_file(self, file_path, test_container=None):
+        """
+        delete given test file
+        :param file_path: absolute path
+        :param test_container: optional test container, else will be
+            determined
+        """
         test_container = test_container or self.cluster.nodes[self.master][
             'docker_cli'
         ].containers.get(self.app.ct.test)
@@ -105,10 +117,18 @@ class WhenDeployingANewServiceMasterSlaveWithNotAnyUpdate(UpdateScriptBase):
         )
         self.cluster.wait_http_code(timeout=10)
 
+    def post_migrate_script_should_be_deployed(self):
+        self._is_file_deployed(self.__class__.migrate_script_name)
+
+    def but_post_migrate_script_should_not_be_run(self):
+        assert self._get_test_file_content(
+            self.__class__.migrate_test_file_path
+        ) == ''
+
     def update_script_should_be_deployed(self):
         self._is_file_deployed(self.__class__.update_script_name)
 
-    def update_script_should_not_be_run(self):
+    def but_update_script_should_not_be_run(self):
         assert self._get_test_file_content(
             self.__class__.update_test_file_path
         ) == ''
@@ -122,9 +142,8 @@ class WhenDeployingANewServiceMasterSlaveWithUpdate(UpdateScriptBase):
     def given_a_cluster_without_test_service(self):
         self.application = cluster.Application(
             'https://github.com/mlfmonde/cluster_lab_test_service',
-            #'master'
-            'update_script'  # TODO merge update_script to master
-                             # and reactive master
+            'update_script'  # testing through specific branch
+                             # with dedicated post migrate script
         )
         self.cluster.cleanup_application(self.application)
         self.master = 'node1'
@@ -148,10 +167,10 @@ class WhenDeployingANewServiceMasterSlaveWithUpdate(UpdateScriptBase):
         self.cluster.wait_http_code(timeout=10)
 
     def post_migrate_script_should_be_deployed(self):
-        # post_migrate should be applied before update script
+        # post_migrate should be deployed
         self._is_file_deployed(self.__class__.migrate_script_name)
 
-    def post_migrate_script_should_be_run(self):
+    def and_post_migrate_script_should_be_run(self):
         # post_migrate should be applied before update script
         assert self._get_test_file_content(
             self.__class__.migrate_test_file_path
@@ -160,7 +179,7 @@ class WhenDeployingANewServiceMasterSlaveWithUpdate(UpdateScriptBase):
     def update_script_should_be_deployed(self):
         self._is_file_deployed(self.__class__.update_script_name)
 
-    def update_script_should_be_run(self):
+    def and_update_script_should_be_run(self):
         assert self._get_test_file_content(
             self.__class__.update_test_file_path
         ).startswith(self.__class__.update_test_file_path)
